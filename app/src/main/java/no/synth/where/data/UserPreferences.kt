23347ce0223c33d23fa4dbc18a381d1ad156a095
henrik242 <henrik242@users.noreply.github.com>
2026-01-05 19:16:03 +0100

    tweak

--- app/src/main/java/no/synth/where/data/UserPreferences.kt
+++ app/src/main/java/no/synth/where/data/UserPreferences.kt
@@ -7,8 +7,8 @@ import androidx.compose.runtime.setValue
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
