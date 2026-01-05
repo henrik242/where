2d0f065cf78ccd3ebadab3672bffe208e68153b7
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-01-05 13:31:12 +0100

    ditch unnecessary permissions

--- app/src/main/java/no/synth/where/ui/MapScreen.kt
+++ app/src/main/java/no/synth/where/ui/MapScreen.kt
@@ -232,8 +232,7 @@ fun MapScreen(
         permissionLauncher.launch(
             arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                Manifest.permission.ACCESS_COARSE_LOCATION
             )
         )
     }
