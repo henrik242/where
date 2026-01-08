01cbb5dc861060fce5528c60feade776fc3ef720
henrik242 <henrik242@users.noreply.github.com>
2026-01-08 07:02:18 +0100

    fix proguard problems with the release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "no.synth.where"
         minSdk = 36
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
