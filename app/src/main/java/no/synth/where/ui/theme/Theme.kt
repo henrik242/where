a77f5eb74706bc2adba0455e898beab0fabed30f
henrik242 <henrik242@users.noreply.github.com>
2026-02-06 07:26:18 +0100

    fix deprecations

--- app/src/main/java/no/synth/where/ui/theme/Theme.kt
+++ app/src/main/java/no/synth/where/ui/theme/Theme.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -52,8 +51,6 @@ fun WhereTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = android.graphics.Color.TRANSPARENT
-            window.navigationBarColor = android.graphics.Color.TRANSPARENT
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
             WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
         }
