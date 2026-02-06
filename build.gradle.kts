c6ac2a9f60a7043a3d7abb75d6f8a44483a820d4
henrik242 <henrik242@users.noreply.github.com>
2026-02-06 16:53:44 +0100

    more version upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 }
+
+buildscript {
+    dependencies {
+        // Override kotlin version in AGP
+        // https://developer.android.com/build/releases/agp-9-0-0-release-notes#runtime-dependency-on-kotlin-gradle-plugin
+        classpath(libs.kotlin.gradle.plugin)
+    }
+}
