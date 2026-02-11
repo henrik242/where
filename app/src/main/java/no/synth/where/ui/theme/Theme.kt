d8933dc219862dde6b17e73652675bd87fd440c0
henrik242 <henrik242@users.noreply.github.com>
2026-02-11 16:46:34 +0100

    Address deprecated edge-to-edge handling

--- app/src/main/java/no/synth/where/ui/theme/Theme.kt
+++ app/src/main/java/no/synth/where/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package no.synth.where.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xFFBB86FC),
@@ -47,15 +43,6 @@ fun WhereTheme(
         else -> LightColorScheme
     }
     
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
-            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
-        }
-    }
-
     MaterialTheme(
         colorScheme = colorScheme,
         content = content
