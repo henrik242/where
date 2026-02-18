d80a77019f84253c77fdefba43de3b05c2867a31
henrik242 <henrik242@users.noreply.github.com>
2026-02-18 06:12:57 +0100

    Wire Firebase Crashlytics on iOS via Swift bridge pattern

--- iosApp/iosApp/WhereApp.swift
+++ iosApp/iosApp/WhereApp.swift
@@ -1,9 +1,23 @@
 import SwiftUI
 import Shared
+import FirebaseCore
+import FirebaseCrashlytics
+
+class IosCrashReporterBridge: CrashReporterBridge {
+    func setEnabled(enabled: Bool) {
+        Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(enabled)
+    }
+
+    func log(message: String) {
+        Crashlytics.crashlytics().log(message)
+    }
+}
 
 @main
 struct WhereApp: App {
     init() {
+        FirebaseApp.configure()
+        CrashReporter.shared.bridge = IosCrashReporterBridge()
         KoinHelperKt.doInitKoin()
     }
 
--- shared/src/iosMain/kotlin/no/synth/where/util/CrashReporter.kt
+++ shared/src/iosMain/kotlin/no/synth/where/util/CrashReporter.kt
@@ -1,11 +1,18 @@
 package no.synth.where.util
 
+interface CrashReporterBridge {
+    fun setEnabled(enabled: Boolean)
+    fun log(message: String)
+}
+
 actual object CrashReporter {
+    var bridge: CrashReporterBridge? = null
+
     actual fun setEnabled(enabled: Boolean) {
-        Logger.d("CrashReporter: setEnabled(%s)", enabled.toString())
+        bridge?.setEnabled(enabled) ?: Logger.d("CrashReporter: setEnabled(%s)", enabled.toString())
     }
 
     actual fun log(message: String) {
-        Logger.d("CrashReporter: %s", message)
+        bridge?.log(message) ?: Logger.d("CrashReporter: %s", message)
     }
 }
